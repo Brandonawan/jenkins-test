e2bed463caf9601f3307ea0d68822509859728f0
Brandon Awan <abibangbrandon855@gmail.com>
2023-06-12 17:10:53 +0100

    fixing the error of chrome path

--- hello.py
+++ hello.py
@@ -4,7 +4,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 # Set up the Chrome WebDriver (you can use other browsers as well)
-driver = webdriver.Chrome()
+driver = webdriver.Chrome('/usr/lib/chromium-browser/chromedriver')
 
 # Navigate to the website you want to test
 driver.get('https://www.example.com')
