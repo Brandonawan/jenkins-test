c5b2325697996ed7849d0ec52791527f64ba5c69
Brandon Awan <abibangbrandon855@gmail.com>
2023-06-12 17:05:48 +0100

    first selenuim code to test

--- hello.py
+++ hello.py
@@ -1,1 +1,12 @@
-print("Hello world!")
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+# Set up the Chrome WebDriver (you can use other browsers as well)
+driver = webdriver.Chrome()
+
+# Navigate to the website you want to test
+driver.get('https://www.example.com')
+
+driver.quit()
