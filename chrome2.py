61ab0f1966361839940379e364dfaffe3fed7b79
Brandon Awan <abibangbrandon855@gmail.com>
2023-06-12 20:58:53 +0100

    update

--- chrome2.py
+++ chrome2.py
@@ -1,20 +1,24 @@
 import time
-
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
-
+from selenium.webdriver.chrome.options import Options
 
 # Set the path to the ChromeDriver executable
 chrome_driver_path = '/usr/bin/chromedriver'
 
+# Set up Chrome options
+chrome_options = Options()
+chrome_options.add_argument('--headless')  # Enable headless mode
+
 # Set up ChromeDriver service
 service = Service(chrome_driver_path)
 
-# Set up Chrome WebDriver with the service
-driver = webdriver.Chrome(service=service) # Optional argument, if not specified will search path.
+# Set up Chrome WebDriver with the service and options
+driver = webdriver.Chrome(service=service, options=chrome_options)
 
 driver.get('http://www.google.com/')
 
-time.sleep(2) # Let the user actually see something!
+time.sleep(2)  # Let the user actually see something!
 
+print("Completed successfully!..")
 driver.quit()
