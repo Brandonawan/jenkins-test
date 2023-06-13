eb6a92a16b86040f54c37005f1db2fc4c53c8681
Brandon Awan <abibangbrandon855@gmail.com>
2023-06-13 17:04:16 +0100

    updates

--- chrome.py
+++ chrome.py
@@ -1,20 +1,18 @@
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
 
 # Set the path to the ChromeDriver executable
 chrome_driver_path = '/usr/bin/chromedriver'
 
-# Set up ChromeDriver service
-service = Service(chrome_driver_path)
-
-# Set up Chrome WebDriver with the service
-driver = webdriver.Chrome(service=service)
+# Set up Chrome WebDriver
+driver = webdriver.Chrome(executable_path=chrome_driver_path)
 
 # Navigate to the website you want to test
 driver.get('https://www.example.com')
 
 # Perform your Selenium actions
 # ...
+
+print("Completed successfully!..")
 
 # Quit the driver
 driver.quit()
