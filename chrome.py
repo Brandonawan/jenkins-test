cdbf7414ca36b959d1dd49c20f13a147aab3eecc
Brandon Awan <abibangbrandon855@gmail.com>
2023-06-15 18:30:43 +0100

    updates

--- chrome.py
+++ chrome.py
@@ -1,42 +1,45 @@
 import time
-from selenium import webdriver
-from selenium.webdriver.firefox.service import Service as FirefoxService
-from webdriver_manager.firefox import GeckoDriverManager
-from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.select import Select
 
-# Set up Firefox options
-firefox_options = Options()
-firefox_options.headless = True  # Run Firefox in headless mode for better performance
+from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.firefox.service import Service
+from webdriver_manager.firefox import GeckoDriverManager
 
-# Set up Firefox WebDriver with the options
-driver_service = FirefoxService(executable_path=GeckoDriverManager().install())
-driver = webdriver.Firefox(service=driver_service, options=firefox_options)
+base_url = "http://3.109.132.239:8080/"
+# Set headless mode options
+options = Options()
+options.add_argument('-headless')
 
-# Set maximum wait time for explicit waits
-wait = WebDriverWait(driver, 10)
+# Set up the Firefox driver service
+service = Service(GeckoDriverManager().install())
 
-base_url = "http://3.109.132.239:8080/"  
-driver.get(f"{base_url}auth/login")
+# Set up the Firefox driver
+driver = webdriver.Firefox(service=service, options=options)
 
-# Login
-username_input = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, "input[name='username']")))
-username_input.send_keys("ccadmin")
-password_input = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, "input[name='password']")))
-password_input.send_keys("admin#123")
-login_button = wait.until(EC.element_to_be_clickable((By.ID, "submit")))
+# Maximize the browser window
+driver.maximize_window()
+
+# Navigate to the app login page
+driver.get(base_url+"auth/login")
+
+# Find the username input element and enter the username
+username_input = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, "input[name='username']"))).send_keys("ccadmin")
+
+# Find the password input element and enter the password
+password_input = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, "input[name='password']"))).send_keys("admin#123")
+
+# Find the login button and click it
+login_button = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.ID, "submit")))
 login_button.click()
 
 # Wait for the dashboard page to load
-dashboard_url = base_url + "dashboard"
-wait.until(EC.url_to_be(dashboard_url))
+dashboard_url = base_url+"dashboard"
+WebDriverWait(driver, 10).until(EC.url_to_be(dashboard_url))
 assert driver.current_url == dashboard_url
 print(f"✓ Login successful to: {dashboard_url}")
-
-# Set the path to the chromedriver binar
 
 # Find the wcagDb link in the sidebar and click it
 wcagdb_link = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "a[href='/wcagDB']")))
