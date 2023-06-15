31cf512d1ea76823bcf89378ec261bb2ec305548
Brandon Awan <abibangbrandon855@gmail.com>
2023-06-15 16:06:25 +0100

    updates

--- chrome2.py
+++ chrome2.py
@@ -1,38 +1,3 @@
-import time
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service as ChromeService
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.chrome.options import Options
-
-# Set up Chrome options
-chrome_options = Options()
-chrome_options.add_argument('--headless')  # Enable headless mode
-
-# Set up Chrome WebDriver with the options
-driver_service = ChromeService(ChromeDriverManager().install())
-driver = webdriver.Chrome(service=driver_service, options=chrome_options)
-
-# Open a website
-driver.get('http://www.google.com/')
-
-time.sleep(5)  # Let the page load
-
-# Perform some actions
-search_box = driver.find_element('name', 'q')
-search_box.send_keys('Selenium 4')
-search_box.submit()
-
-time.sleep(5)  # Let the search results load
-
-# Get the search results and print the titles
-search_results = driver.find_elements('xpath', '//h3')
-for result in search_results:
-    print(result.text)
-
-# Quit the WebDriver
-driver.quit()
-
-
 # import time
 # from selenium import webdriver
 # from selenium.webdriver.chrome.service import Service as ChromeService
@@ -41,11 +6,11 @@ driver.quit()
 
 # # Set up Chrome options
 # chrome_options = Options()
-# chrome_options.headless = True  # Enable headless mode
+# chrome_options.add_argument('--headless')  # Enable headless mode
 
 # # Set up Chrome WebDriver with the options
-# driver_service = ChromeService(executable_path=ChromeDriverManager().install())
-# driver = webdriver.Chrome(options=chrome_options)
+# driver_service = ChromeService(ChromeDriverManager().install())
+# driver = webdriver.Chrome(service=driver_service, options=chrome_options)
 
 # # Open a website
 # driver.get('http://www.google.com/')
@@ -66,4 +31,39 @@ driver.quit()
 
 # # Quit the WebDriver
 # driver.quit()
+
+
+import time
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.options import Options
+
+# Set up Chrome options
+chrome_options = Options()
+chrome_options.headless = True  # Enable headless mode
+
+# Set up Chrome WebDriver with the options
+driver_service = ChromeService(executable_path=ChromeDriverManager().install())
+driver = webdriver.Chrome(options=chrome_options)
+
+# Open a website
+driver.get('http://www.google.com/')
+
+time.sleep(5)  # Let the page load
+
+# Perform some actions
+search_box = driver.find_element('name', 'q')
+search_box.send_keys('Selenium 4')
+search_box.submit()
+
+time.sleep(5)  # Let the search results load
+
+# Get the search results and print the titles
+search_results = driver.find_elements('xpath', '//h3')
+for result in search_results:
+    print(result.text)
+
+# Quit the WebDriver
+driver.quit()
 
