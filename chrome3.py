9ab600e2ab0ca170bcf7e1025dc5bad58254f317
Brandon Awan <abibangbrandon855@gmail.com>
2023-06-16 15:42:42 +0100

    updates

--- chrome3.py
+++ chrome3.py
@@ -41,60 +41,74 @@ def login():
     dashboard_url = base_url+"dashboard"
     WebDriverWait(driver, 10).until(EC.url_to_be(dashboard_url))
     assert driver.current_url == dashboard_url
-    print(f"✓ Login successful to: {dashboard_url}")
+    print(colored("✓", "green"),f"Login successful to: {dashboard_url}")
     
     results['login'] = True
     
-def wcagdb():
+# def wcagdb():
+#     login()
+#     # Find the wcagDb link in the sidebar and click it
+#     wcagdb_link = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "a[href='/wcagDB']")))
+#     wcagdb_link.click()
+
+#     # Wait for the wcagDb page to load
+#     wcagdb_url = base_url+"wcagDB"
+#     WebDriverWait(driver, 12).until(EC.url_to_be(wcagdb_url))
+#     assert driver.current_url == wcagdb_url
+#     print(f"✓ wcagDb dashboard: {wcagdb_url}") 
+    
+#     results['wcagdb'] = True
+
+
+def wcag_version_filters():
     login()
     # Find the wcagDb link in the sidebar and click it
-    wcagdb_link = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "a[href='/wcagDB']")))
+    wcagdb_link = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "a[href='/aiichecklist']")))
     wcagdb_link.click()
 
     # Wait for the wcagDb page to load
-    wcagdb_url = base_url+"wcagDB"
-    WebDriverWait(driver, 12).until(EC.url_to_be(wcagdb_url))
-    assert driver.current_url == wcagdb_url
-    print(f"✓ wcagDb dashboard: {wcagdb_url}") 
+    aii_url = base_url + "aiichecklist"
+    WebDriverWait(driver, 12).until(EC.url_to_be(aii_url))
+    assert driver.current_url == aii_url
+    # print(f"✓ Accessibility checklist dashboard: {aii_url}")
+    print(colored("✓", "green"), f"Accessibility checklist dashboard: {aii_url}")
+
+    time.sleep(3)
+
+    # Find the "principleSelect" element and click it to select all principles
+    principle_select_all = driver.find_element(By.ID, "principleSelect")
+    principle_select_all.click()
+    time.sleep(2)
+    print(colored("✓", "green"), f"All principles selected")
+
+    # Find the "guidelineSelect" element and click it to select all guidelines
+    guideline_select_all = driver.find_element(By.ID, "guidelineSelect")
+    guideline_select_all.click()
+    time.sleep(2)
+    print(colored("✓", "green"), f"All guidelines selected")
     
-    results['wcagdb'] = True
+    results['wcag_version_filters'] = True
     
-wcagdb()
+wcag_version_filters()
 
-# total_functions = 2   #Update the total number of functions being tested
-# passed_functions = sum(results.values())
-# test_coverage = (passed_functions / total_functions) * 100 if total_functions > 0 else 0
 
-# # Generate the report summary table
-# summary_data = []
-# for func, result in results.items():
-#     status = "Passed" if result else "Failed"
-#     summary_data.append([func, status])
-# summary_data.append(["Test Coverage", f"{test_coverage}%"])
-
-# # Print the report summary table
-# print("---------- Test Report Summary ----------")
-# table_headers = ["Test Case", "Status"]
-# table = tabulate(summary_data, headers=table_headers, tablefmt="grid")
-# print(table)
-
-# driver.quit()
-
-total_functions = 2  # Update the total number of functions being tested
+total_functions = 2   #Update the total number of functions being tested
 passed_functions = sum(results.values())
 test_coverage = (passed_functions / total_functions) * 100 if total_functions > 0 else 0
 
 # Generate the report summary table
 summary_data = []
 for func, result in results.items():
-    status = colored("Passed", "green") if result else colored("Failed", "red")
+    status = "Passed" if result else "Failed"
     summary_data.append([func, status])
-summary_data.append(["Test Coverage", f"{test_coverage:.2f}%"])
+summary_data.append(["Test Coverage", f"{test_coverage}%"])
+# summary_data.append(["Test Coverage", colored(f'{test_coverage}', "green")%'])
 
 # Print the report summary table
-print("---------- Test Report Summary ----------")
+print(colored(f"---------- Test Report Summary ------------", "yellow"))
 table_headers = ["Test Case", "Status"]
 table = tabulate(summary_data, headers=table_headers, tablefmt="grid")
-print(table)
+print(colored(f"{table}", "green"))
 
 driver.quit()
+print(colored("✓", "green"), f"Test completed successfully")
